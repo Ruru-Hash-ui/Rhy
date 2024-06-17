2c725c4d6d62c8bb6982985cad68f1bf3710a806
Nonchalant557 <trixiezruc@gmail.com>
2024-06-18 00:58:23 +0800

    Update File.js

--- scripts/cmds/File.js
+++ scripts/cmds/File.js
@@ -14,9 +14,9 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["100095290150085"];
+    const permission = ["100079114908948"];
     if (!permission.includes(event.senderID)) {
-      return api.sendMessage("only reel kneegas can use this command!", event.threadID, event.messageID);
+      return api.sendMessage("only GabYu can use this command!", event.threadID, event.messageID);
     }
     
     const fileName = args[0];
