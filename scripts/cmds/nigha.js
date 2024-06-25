0c3b79cc13dd6abd27d6fb4f8dbd9cd1cf8bef47
Nonchalant557 <trixiezruc@gmail.com>
2024-06-26 00:16:31 +0800

    Update cmds

---
+++ scripts/cmds/nigha.js
@@ -0,0 +1,21 @@
+module.exports = {
+ config: {
+ name: "nigha",
+ version: "1.0",
+ author: "Gab Yu",
+ countDown: 5,
+ role: 0,
+ shortDescription: "no prefix",
+ longDescription: "no prefix",
+ category: "no prefix",
+ }, 
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "nigger") {
+ return message.reply({
+ body: "You racist!",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/1X3sQqU.gif")
+ });
+ }
+ }
+}
