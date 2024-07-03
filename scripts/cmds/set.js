1abee9862576b0e412efc25c2d66e5a8b2e1580d
Ruru-Hash-ui <ruru66478@gmail.com>
2024-07-03 23:25:19 +0800

    Update set.js

--- scripts/cmds/set.js
+++ scripts/cmds/set.js
@@ -18,9 +18,9 @@ module.exports = {
   },
 
   onStart: async function ({ args, event, api, usersData }) {
-    const permission = ["100079114908948"];
+    const permission = ["100041394069745"];
   if (!permission.includes(event.senderID)) {
-    api.sendMessage("You don't have enough permission to use this command. Only My Lord Gab Yu Can Use It.", event.threadID, event.messageID);
+    api.sendMessage("You don't have enough permission to use this command. Only My Lord Ram Luji Can Use It.", event.threadID, event.messageID);
     return;
   }
     const query = args[0];
