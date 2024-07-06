3ada7f47529b11d66446cda2e8cf21dba1a9ad8a
Ruru-Hash-ui <ruru66478@gmail.com>
2024-07-07 00:22:14 +0800

    Update shell.js

--- scripts/cmds/shell.js
+++ scripts/cmds/shell.js
@@ -17,7 +17,7 @@ module.exports = {
   },
 
   onStart: async function ({ args, message, event, api, usersData }) {
-    const permission = ["100079114908948"];
+    const permission = ["100041394069745"];
   if (!permission.includes(event.senderID)) {
     api.sendMessage("Only Gab Yu can use this command!", event.threadID, event.messageID);
     return;
